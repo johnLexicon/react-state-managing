14254b51a97a581bd530c7f1ecb14586a2f5466d
John Lundgren <45228836+johnLexicon@users.noreply.github.com>
2021-12-29 10:45:40 +0100

    Fetching and filtering products.

--- 03/demos/before/src/App.jsx
+++ 03/demos/before/src/App.jsx
@@ -1,20 +1,52 @@
-import React from "react";
-import "./App.css";
-import Footer from "./Footer";
-import Header from "./Header";
+import React, { useState, useEffect } from 'react';
+import { getProducts } from './services/productService';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
 
 export default function App() {
-  // function renderProduct(p) {
-  //   return (
-  //     <div key={p.id} className="product">
-  //       <a href="/">
-  //         <img src={`/images/${p.image}`} alt={p.name} />
-  //         <h3>{p.name}</h3>
-  //         <p>${p.price}</p>
-  //       </a>
-  //     </div>
-  //   );
-  // }
+  const [products, setProducts] = useState([]);
+  const [productsBySize, setProductsBySize] = useState([]);
+  const [currentSize, setCurrentSize] = useState('');
+
+  const retrieveProducts = async () => {
+    try {
+      const fetchedProducts = await getProducts('shoes');
+      setProducts(fetchedProducts);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const filteredBySize = () => {
+    if (currentSize === '') {
+      setProductsBySize((prevState) => [...products]);
+      return;
+    }
+    setProductsBySize((prevState) =>
+      products.filter((p) => p.skus.find((s) => +s.size === +currentSize))
+    );
+  };
+
+  useEffect(() => {
+    retrieveProducts();
+  }, []);
+
+  useEffect(() => {
+    filteredBySize();
+  }, [products, currentSize]);
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -22,14 +54,21 @@ export default function App() {
         <Header />
         <main>
           <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size">
+            <label htmlFor="size">Filter by Size:</label>{' '}
+            <select
+              id="size"
+              value={currentSize}
+              onChange={(e) => {
+                setCurrentSize(e.target.value);
+              }}
+            >
               <option value="">All sizes</option>
               <option value="7">7</option>
               <option value="8">8</option>
               <option value="9">9</option>
             </select>
           </section>
+          <section id="products">{productsBySize.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
