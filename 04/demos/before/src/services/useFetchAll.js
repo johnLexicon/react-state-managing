e9422ffdc14adb2567dadd2b69e67755c0423c2b
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-01-02 12:22:58 +0100

    Urls check to avoid unecessary fetchs.

--- 04/demos/before/src/services/useFetchAll.js
+++ 04/demos/before/src/services/useFetchAll.js
@@ -1,13 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
+// Function is put outside the hook to avoid reallocation of the function every time a compoment is rerendered.
+function areEqual(arr1, arr2) {
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => value === arr2[index])
+  );
+}
 
 export default function useFetchAll(urls) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const prevUrlsRef = useRef([]); // To check if the url array has changed since last time.
 
   useEffect(() => {
+    // Abort if the urls are equal to previous passed in urls
+    if (areEqual(prevUrlsRef.current, urls)) return;
+    prevUrlsRef.current = urls; // Assign the latest url array
+
     const promises = urls.map((url) => {
       return new Promise((resolve, reject) => {
         fetch(baseUrl + url).then((response) => {
@@ -24,7 +37,7 @@ export default function useFetchAll(urls) {
       .then((results) => setData(results))
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
-  }, []);
+  }, [urls]);
 
   return { data, loading, error };
 }
